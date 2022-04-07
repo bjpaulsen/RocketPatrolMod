fd484e35080fdbde2c732638da8b12b6614b3484
Ben Paulsen <benpaulsen@MacBook-Pro.local>
2022-04-07 12:45:55 -0700

    making sure Play.js matches tutorial

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,17 +8,14 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-        this.add.tileSprite(0, 0, game.config.width, game.config.height, "starfield").setOrigin(0, 0)
-
-        this.add.text(20, 20, "play mode bb");
-
+        // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
-
+        
+        // white borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.width, 0xFFFFFF).setOrigin(0, 0);
-
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
 
     }
 }
