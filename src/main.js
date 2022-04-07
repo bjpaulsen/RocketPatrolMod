896667771fc8641ea76d5d8e20308f6e9197d287
Ben Paulsen <benpaulsen@MacBook-Pro.local>
2022-04-07 12:44:57 -0700

    fix border settings

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ let config = {
     scene: [Menu, Play],
 }
 
-let borderUISize = 20;
-let borderPadding = 20;
-
 let game = new Phaser.Game(config);
+
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
