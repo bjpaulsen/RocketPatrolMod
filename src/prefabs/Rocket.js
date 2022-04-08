b2dad00ad081e775849088ef97f244c5741560a8
Ben Paulsen <benpaulsen@MacBook-Pro.local>
2022-04-08 15:32:00 -0700

    implement basic rocket control

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -2,5 +2,22 @@ class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture) {
         super(scene, x, y, texture);
         scene.add.existing(this);
+        this.isFiring = false;
     }
+
+    update() {
+        if (this.isFiring) {
+            this.y -= 10;
+
+            if (this.y < 0) {
+                this.reset();
+            }
+        }
+    }
+
+    reset() {
+        this.y = 431;
+        this.isFiring = false;
+    }
+    
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,6 +10,12 @@ class Play extends Phaser.Scene {
     }
 
     create() {
+        // define keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
         // place tile sprite
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
 
@@ -22,10 +28,22 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
 
-        let p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket');
+        this.p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket').setOrigin(0.5, 0);
+
+        this.add.sprite(game.config.width/2, 200, 'spaceship');
     }
 
     update() {
         // this.starfield.tilePositionX -= 4;
-      }
+        let rocketSpeed = 4;
+        if (keyLEFT.isDown)
+            this.p1Rocket.x -= rocketSpeed
+        if (keyRIGHT.isDown)
+            this.p1Rocket.x += rocketSpeed
+        
+        if (Phaser.Input.Keyboard.JustDown(keyF))
+            this.p1Rocket.isFiring = true;
+
+        this.p1Rocket.update();
+    }
 }
