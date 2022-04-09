ca0df93ac88c5fe66c4a63bc2232239a89cbace7
Ben Paulsen <benpaulsen@MacBook-Pro.local>
2022-04-09 14:24:50 -0700

    create menu visual

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,7 +2,7 @@ class Menu extends Phaser.Scene {
     constructor() {
         super("menu");
     }
-    
+
     preload() {
         // load audio
         this.load.audio('sfx_select', './assets/blip_select12.wav');
@@ -11,8 +11,28 @@ class Menu extends Phaser.Scene {
       }
 
     create() {
-        let menuText = this.add.text(300, 240, "menu here");
-        menuText.setOrigin(.5, .5);
-        this.scene.start("play");
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+            top: 5,
+            bottom: 5,
+            },
+            fixedWidth: 0
+        }
+
+        // show menu text
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(.5);
+        this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(.5);
+        menuConfig.backgroundColor = '#00FF00';
+        menuConfig.color = '#000000';
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(.5);
+    
+        // define keys
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -80,6 +80,7 @@ class Play extends Phaser.Scene {
             this.scene.restart();
         }
 
+        // scroll starfield
         this.starfield.tilePositionX -= 4;
 
         if (!this.gameOver) {    
